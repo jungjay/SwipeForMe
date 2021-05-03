2c35ab885b4a51495e199aa26748be5b21ab4f96
Jay Jung <313jj2@gmail.com>
2021-05-02 21:58:06 -0400

    Initial Commit

---
+++ SwipeForMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeForMe
+//
+//  Created by Jay Jung on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
